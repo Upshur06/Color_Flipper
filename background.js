5edcb2e786839fe7c71c8bccd4927a4848aebbac
Upshur06 <mupshur2005@gmail.com>
2020-07-18 19:20:52 -0400

    button and testing color

--- background.js
+++ background.js
@@ -3,3 +3,15 @@ let colors = ["green","red","rgba(133,122,200)","#f1505"];
 let btn = document.getElementById("btn");
 
 let color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    let randomNumber = getRandomNumber(0,3);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.tectContent = color[randomNumber];
+})
+
+function getRandomNumber(min,max){
+    var number = parseInt(Math.random() * (max - min) +min);
+    console.log(number);
+    return number;
+}
