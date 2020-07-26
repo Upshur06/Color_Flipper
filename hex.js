454158e1cf0a155f243a184fd92406204affb40a
Upshur06 <mupshur2005@gmail.com>
2020-07-26 04:57:46 -0400

    random hexColor

--- hex.js
+++ hex.js
@@ -6,8 +6,12 @@ let color = document.querySelector('.color');
 btn.addEventListener('click',function(){
   let hexColor = '#';
   for(let i=0;i<6;i++){
-    hexColor += hex[i];
+    hexColor += hex[getRandomNumber()];
   }
   color.textContent = hexColor;
   document.body.style.backgroundColor = hexColor;
 })
+
+function getRandomNumber(){
+  return Math.floor(Math.random() * hex.length);
+}
